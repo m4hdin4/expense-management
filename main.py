0b27fa3e35188b7bf575b5c7bde080dee1c689c7
m4hdin4 <mahdinasri127@gmail.com>
2021-05-26 21:57:19 +0430

    Basic template created

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from flask import Flask
+import sys
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/get', methods=['GET'])
+def get_list():
+    return sys._getframe().f_code.co_name
+
+
+@app.route('/get/<spend_id>', methods=['GET'])
+def get_one(spend_id):
+    return sys._getframe().f_code.co_name
+
+
+
+@app.route('/post/<product_name>/<product_price>', methods=['POST'])
+def post_one(product_name, product_price):
+    return sys._getframe().f_code.co_name
+
+
+@app.route('/put/<spend_id>/<product_name>/<product_price>', methods=['PUT'])
+def update(spend_id, product_name, product_price):
+    return sys._getframe().f_code.co_name
+
+
+@app.route('/delete/<spend_id>', methods=['DELETE'])
+def delete(spend_id):
+    return sys._getframe().f_code.co_name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
