63c5f9f9867aae6fa346f4df7a103a8921fac349
m4hdin4 <mahdinasri127@gmail.com>
2021-05-29 21:20:34 +0430

    category based project completed(faze 3)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-
 from flask import Flask
 from flask import request
 from flask import abort
@@ -17,9 +16,13 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
+class Category(Document):
+    category_name = StringField(required=True, max_length=40, unique=True)
+
+
 class Item(Document):
     product_name = StringField(required=True, max_length=40)
-    category = StringField(required=True, max_length=40)
+    category = ReferenceField(Category, required=True)
     product_price = IntField(required=True)
     date = DateTimeField(default=datetime.datetime.now())
 
@@ -31,12 +34,10 @@ def get_list():
     for it in Item.objects:
         single_item = {"id": it.id,
                        "product_name": it.product_name,
-                       "category": it.category,
+                       "category": it.category.category_name,
                        "product_price": it.product_price,
                        "date": str(it.date)}
         output_list.append(single_item)
-    if not output_list:
-        abort(404)
     output_sum = Item.objects.sum('product_price')
     output = {'list': output_list, 'sum': output_sum}
     return JSONEncoder().encode(output), 200
@@ -48,7 +49,7 @@ def get_one(spend_id):
         it = Item.objects(id=spend_id).first()
         output = {'list': [{"id": it.id,
                             "product_name": it.product_name,
-                            "category": it.category,
+                            "category": it.category.category_name,
                             "product_price": it.product_price,
                             "date": str(it.date)}],
                   'sum': it.product_price}
@@ -57,8 +58,24 @@ def get_one(spend_id):
         abort(404)
 
 
+@app.route('/get/category/<category>', methods=['GET'])
+def get_category(category):
+    category_item = Category.objects(category_name=category).first()
+    output_list = []
+    for it in Item.objects(category=category_item):
+        single_item = {"id": it.id,
+                       "product_name": it.product_name,
+                       "category": it.category.category_name,
+                       "product_price": it.product_price,
+                       "date": str(it.date)}
+        output_list.append(single_item)
+    output_sum = Item.objects(category=category_item).sum('product_price')
+    output = {'list': output_list, 'sum': output_sum}
+    return JSONEncoder().encode(output), 200
+
+
 @app.route('/post', methods=['POST'])
-def post_one():
+def insert():
     if not request.json or \
             'product_name' not in request.json or \
             'product_price' not in request.json or \
@@ -68,7 +85,12 @@ def post_one():
     product_price = request.json['product_price']
     category = request.json['category']
     try:
-        inserted = Item(product_name=product_name, product_price=product_price, category=category).save()
+        category_item = Category.objects(category_name=category)[0]
+        print("ok")
+    except:
+        category_item = Category(category_name=category).save()
+    try:
+        inserted = Item(product_name=product_name, product_price=product_price, category=category_item).save()
         return get_one(inserted.id)
     except:
         abort(400)
@@ -86,16 +108,35 @@ def update():
     product_price = request.json['product_price']
     category = request.json['category']
     try:
+        category_item = Category.objects(category_name=category).first()
+    except:
+        category_item = Category(category_name=category).save()
+    try:
         Item.objects(id=spend_id)[0]
     except:
         abort(404)
     try:
         Item.objects(id=spend_id).update_one(set__product_name=product_name,
                                              set__product_price=product_price,
-                                             set__category=category)
+                                             set__category=category_item)
         return get_one(spend_id)
     except:
         abort(400)
+
+
+@app.route('/put/category', methods=['PUT'])
+def update_category():
+    if not request.json or 'old_category' not in request.json or \
+            'new_category' not in request.json:
+        abort(400)
+    old_category = request.json['old_category']
+    new_category = request.json['new_category']
+
+    flag = Category.objects(category_name=old_category).update(category_name=new_category)
+    if flag == 1:
+        return get_category(new_category)
+    else:
+        abort(404)
 
 
 @app.route('/delete', methods=['DELETE'])
@@ -110,8 +151,21 @@ def delete():
         abort(404)
 
 
+@app.route('/delete/category', methods=['DELETE'])
+def delete_category():
+    if not request.json or 'category' not in request.json:
+        abort(400)
+    category = request.json['category']
+    try:
+        category_item = Category.objects(category_name=category).first()
+        Item.objects(category=category_item).delete()
+        category_item.delete()
+        return 'DELETED'
+    except:
+        abort(404)
+
+
 if __name__ == '__main__':
     connect('spends_db')
-    Item.drop_collection()
     app.run(debug=True)
     disconnect()
