8c9b4361a7d2b059cbf210986c0a1c23461d4e2a
m4hdin4 <mahdinasri127@gmail.com>
2021-06-07 19:47:22 +0430

    apidoc corrected

--- doc/api_project.js
+++ doc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.0",
   "description": "back-end project to manage your expense",
   "title": "expense-management server",
-  "url": "http://127.0.0.1:5000/",
+  "url": "http://127.0.0.1:5000",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-07T13:53:51.025Z",
+    "time": "2021-06-07T15:16:05.217Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
